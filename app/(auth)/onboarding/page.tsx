8bc50e8d20bd7d96e83e8ee8f376daba959d16d2
dialite <raymondolisa01@gmail.com>
2023-10-05 23:58:52 +0100

    Initialize onboarding page and process

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,26 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+  };
   return (
-    <main>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Strands
+      </p>
+
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile />
+      </section>
     </main>
   );
 }
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const AccountProfile = () => {
+  return <div>Account Profile</div>;
+};
+
+export default AccountProfile;
