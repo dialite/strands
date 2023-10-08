bc3da4014162a7da42f001ad43e1763b00e6f15b
dialite <raymondolisa01@gmail.com>
2023-10-08 18:51:05 +0100

    Configure next js to anable communication with external images from clerk for user profiole

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,10 +35,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
 
