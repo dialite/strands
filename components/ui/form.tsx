8343e3bc66baafa88396838ee3cd7bd38107c653
dialite <raymondolisa01@gmail.com>
2023-10-25 22:49:48 +0100

    Testing styles

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -47,7 +47,7 @@ const useFormField = () => {
   const fieldState = getFieldState(fieldContext.name, formState);
 
   if (!fieldContext) {
-    throw new Error("useFormField should be used strictly within <FormField>");
+    throw new Error("useFormField should be used within <FormField>");
   }
 
   const { id } = itemContext;
